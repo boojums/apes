d98285254b9e0da9870c3a960c3fd8c02eefa86c
Cristina <cristina.luis@gmail.com>
2014-11-17 08:36:47 +0100

    changed to browserAction

--- background.js
+++ background.js
@@ -1,11 +1,12 @@
 // Copyright (c) 2014 Cristina Luis
 
+
 // Called when the url of a tab changes.
 function checkForAP(tabId, changeInfo, tab) {
     // If the string 'attackpoint.org' is found in the tab's URL...
     if (tab.url.indexOf("attackpoint.org") > -1) {
         // ... show the page action.
-        chrome.pageAction.show(tabId);
+        chrome.browserAction.show(tabId);
     }
 }
 
@@ -31,7 +32,7 @@ function openOrFocusOptionsPage() {
 }
 
 // Called when the user clicks on the browser action.
-chrome.pageAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function(tab) {
     openOrFocusOptionsPage();
 });
 
--- options.js
+++ options.js
@@ -4,12 +4,14 @@
     - chickenUsers: [userid] 
 */
 
+// TODO: css for options.html so that links to remove are clicky
+
 //populate()
 // populate data data
 function populate() {
     var hideUsers = ['4380', '470'];
     var chickenUsers = ['100', '5029'];
-    var taggedUsers = {'100': 'old guy', '44': 'old guy'};
+    var taggedUsers = {'100': 'Canadian', '44': 'wise man'};
 
     var settings = {hideUsers: hideUsers, 
                     chickenUsers: chickenUsers,
@@ -28,8 +30,36 @@ function populate() {
     });
 }
 
+// TODO: look up username from:
+// http://attackpoint.org/userprofile.jsp/user_470
 function loadOptions() {
+    chrome.storage.sync.get(null, function(result) {
+        console.log(result);
+            
+        var chickenUsers = result.chickenUsers;
+        var chickenTable = document.getElementById('chickenTable');
+        
+        for (var i=0; i<chickenUsers.length; i++) {
+            var row = chickenTable.insertRow(0);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
 
+            cell1.innerHTML = chickenUsers[i];
+            cell2.innerHTML = '';
+
+            var a = document.createElement('a');
+            a.text = 'x';
+            //a.href = 'options.html';
+            //a.href = 'javascript:void(0)';
+            a.id = '111';
+            console.log(a);
+            a.addEventListener("click", function() {
+                console.log(event.toElement.id);
+            })
+            cell2.appendChild(a);
+            console.log(cell2);
+        }
+    });
 }
 
 function saveOptions() {
@@ -61,5 +91,5 @@ function eraseOptions() {
     location.reload();
 }
 
-//document.addEventListener('DOMContentLoaded', restoreOptions);
+document.addEventListener('DOMContentLoaded', loadOptions);
 document.getElementById('save').addEventListener('click', saveOptions);
