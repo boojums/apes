a885b147b7a48b945cf39fbf5524359ef3d26ba3
Cristina <cristina.luis@gmail.com>
2015-03-25 21:53:17 +0100

    clean up of openfavs

--- openfavs.js
+++ openfavs.js
@@ -3,7 +3,6 @@
     all unread favorites in new tabs.
 */
 
-
 // regex for 'unread: n', which only shows up if there are unread favorites
 // catches anything that isn't 0
 var regex = /unread\:[1-9]/;
@@ -11,31 +10,31 @@ var regex = /unread\:[1-9]/;
 // first see if we are on a page with unread favorites
 if(regex.test(document.body.innerText)) {
     
-    // TODO: chain with jquery instead
     favtable = document.getElementsByClassName('favlist')[0];
     lastrow = favtable.getElementsByClassName('morestuff')[0]; 
-    console.log(lastrow);
     cell = lastrow.cells[0];
 
     var newcontent = document.createElement('a');
     newcontent.text = 'open all';
-    newcontent.href = 'javascript:void(0)';
+    newcontent.href = '#';
     newcontent.id = 'openall'
 
     newcontent.addEventListener("click", function() {
 
-        // Find user id's of unread favorites, listed between unread and 'more logs' link
+        // Find user ids of unread favorites, listed between unread and 'more logs' link
         // Do not want all 'user_n' links as that would include Noisy Logs, etc.
         var chunk = String(document.body.innerHTML.match(/unread[\s\S]*more logs/));    
         var users = chunk.match(/user_[0-9]*/g);
     
-        // the regex produced a match, so notify background script
+        // The regex produced a match, so notify background script
         // and attempt to send it the array of users 
         chrome.runtime.sendMessage({"user_list": users}, function(response) {});
     });
-    console.log(newcontent);
+
     cell.appendChild(document.createTextNode(" | "));
     cell.appendChild(newcontent);
 } else {
-    console.log('nothing to see here...')
+    if (debug) {
+        console.log('nothing to see here...');
+    }
 }
